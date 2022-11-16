14d6518b28c17e460da8bdf1b55fdef124e867c8
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-11-16 16:55:19 -0500

    Update defines.hpp

--- defines.hpp
+++ defines.hpp
@@ -12,7 +12,9 @@
 #define GARC 0x47415243
 #define FILE 0x46494C45
 #define NAME 0x4E414D45
+#define DATA 0x44415441
 
+typedef unsigned char uchar;
 
 ///Logical Block Access info stuffs
 struct lbaSpec {
@@ -22,8 +24,23 @@ struct lbaSpec {
     uint32_t    file_size; //Size of file
 };
 
-///Reverse byte order (32 bits)
-void setReverse(uint32_t &tmpInt);
+static bool isDebug;                   // Whether or not to print messages to console
+static bool hasLog;                    // Whether or not to create log
+static int num_garc;                   // GARC counter
+static int num_gprs;                   // GPRS counter
+static std::vector<lbaSpec> lbaTable;  // Stores LBA information
+
+///Check if bit from array is set
+bool cmpBits(uchar data, int &shift);
+
+///Gets absolute value of variable int from array
+void getAbsInt(int in, int &out);
+
+///Gets LE variable int from array
+void getLeInt(uchar *data, unsigned int &out, int pos, int len);
+
+///Gets BE variable int from array
+void getBeInt(uchar *data, unsigned int &out, int pos, int len);
 
 ///Remove quotation marks from file name
 void removeQuote(std::string &arg);
@@ -38,19 +55,20 @@ void printErr(const char* opt, std::string arg, int code);
 bool fileExists(std::string fName);
 
 ///Extract files from LocoRoco archive
-int fromGARC(std::ofstream &extraction_log, std::ifstream &file, bool isDebug, bool hasLog, int secdex, int seclen, std::string name);
+int fromGARC(std::ofstream &extraction_log, uchar* src, int seclen, std::string name);
 
 ///Inflate file
-int DecryptGPRS(std::ofstream &extraction_log, std::ifstream &section, bool isDebug, bool hasLog, int index, int length, std::string name);
+int inflateGPRS(std::ofstream &extraction_log, uchar* src, int seclen, std::string name);
 
 ///Searches binary file for (compressed) LocoRoco archive(s)
-void searchGARC(std::vector<lbaSpec> table, std::string garc_filename, bool isDebug, bool hasLog);
+void searchGARC(std::string garc_filename);
 
 ///Retrieves LBA table from comma-separated text file
 std::vector<lbaSpec> getTableCSV(std::string csv_filename);
 
 ///Retrieves LBA table from binary file
 std::vector<lbaSpec> getTableBIN(std::string bin_filename);
+std::vector<lbaSpec> getTableBIN(uchar* src);
 
 ///Checks if argument for option is valid
 bool optArgValid(char opt, std::string arg);
