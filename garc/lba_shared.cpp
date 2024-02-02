27e447ea7ca8c244b0c395658a5a077f842ffb0f
Cheena <97012270+Nezogoku@users.noreply.github.com>
2024-02-02 12:02:39 -0500

    Update lba_shared.cpp

--- garc/lba_shared.cpp
+++ garc/lba_shared.cpp
@@ -48,84 +48,89 @@ int lbat::setTableCSV(unsigned char *src, unsigned src_size) {
 
     if (this->isDebug) fprintf(stderr, "    Get LBA table info order\n");
     int nID = -1, rID = -1, sID = -1;
-    for (int t = 0; t < 3; ++t) {
+    for (int t = 0, pos = lbalog.tellPos(); t < 3;) {
         std::string tmp = lbalog.getString(",");
         
         if (tmp.empty()) break;
         if (this->isDebug) fprintf(stderr, "    Log header: %s\n", lbalog.ssub.c_str());
-
-        if (tmp == "FILE_NAME")      nID = t;
-        else if (tmp == "FILE_RLBN") rID = t;
-        else if (tmp == "FILE_SIZE") sID = t;
+        
+             if (tmp == "FILE_NAME") nID = t++;
+        else if (tmp == "FILE_RLBN") rID = t++;
+        else if (tmp == "FILE_SIZE") sID = t++;
+        
+        if (!t) { nID = 0; rID = 1; sID = 2; lbalog.seekPos(pos); break; }
     }
 
-    if (this->isDebug) fprintf(stderr, "    Check if LBA table info order valid\n");
-    if (nID < 0 && rID < 0 && sID < 0) { nID = 0; rID = 1; sID = 2; }
     if (this->isDebug) fprintf(stderr, "    Name ID %i, Block ID %i, Size ID %i\n", nID, rID, sID);
-    if (nID < 0 || rID < 0 || sID < 0) return 0;
+    if ((nID + rID + sID) < 3) { reset(); return 0; }
 
-    if (this->isDebug) fprintf(stderr, "    Resize LBA table to %d\n", tmp.glba_amnt);
-    tmp.glba_info = new lbaSpec::lbainf[tmp.glba_amnt] {};
-
+    if (this->isDebug) fprintf(stderr, "    Resize LBA table to %d\n", this->amnt_glba);
+    this->info = new lbainf[this->amnt_glba] {};
     if (this->isDebug) fprintf(stderr, "    Update LBA table\n");
-    if (this->isDebug) fprintf(stderr, "    Search at position 0x%08X\n", lbalog.str_cur - lbalog.str_beg);
-    for (int m = 0; lbalog.str_beg && m < tmp.glba_amnt; ++m) {
-        bool is_valid = true;
-
-        for (int t = 0; is_valid && t < 3; ++t) {
-            is_valid = lbalog.getStream(",");
-            if (!is_valid) break;
-
-            if (t == nID) tmp.glba_info[m].file_name = lbalog.ssub;
-            else if (t == rID && (int)lbalog.isub >= 0) tmp.glba_info[m].file_rlbn = lbalog.isub;
-            else if (t == sID && (int)lbalog.isub >= 0) tmp.glba_info[m].file_size = lbalog.isub;
-            else is_valid = false;
+    if (this->isDebug) fprintf(stderr, "    Search at position 0x%08X\n", lbalog.tellPos());
+    for (int m = 0; m < this->amnt_glba; ++m) {
+        for (int t = 0; t < 3; ++t) {
+            switch(t) {
+                case nID:
+                    this->info[m].info_name = lbalog.getString(",");
+                    if (this->info[m].info_name.empty()) { this->amnt_glba = m; return 0; }
+                    else break;
+                case rID:
+                    this->info[m].info_rlbn = lbalog.getUnsigned(",");
+                    if ((int)this->info[m].info_rlbn < 0) { this->amnt_glba = m; return 0; }
+                    else break;
+                case sID:
+                    this->info[m].info_size = lbalog.getUnsigned(",");
+                    if ((int)this->info[m].info_size < 0) { this->amnt_glba = m; return 0; }
+                    else break;
+            }
         }
-        if (!is_valid) { tmp.glba_amnt = m; break; }
 
         if (this->isDebug) fprintf(stderr, "        Set %-*s with size 0x%08X to entry %d\n",
-                                           27, tmp.glba_info[m].file_name.c_str(),
-                                           tmp.glba_info[m].file_size,
+                                           27, this->info[m].file_name.c_str(),
+                                           this->info[m].file_size,
                                            m);
     }
-    this->table = tmp;
 
     return 1;
 }
 
 int lbat::setTableBIN(unsigned char *src, unsigned src_size) {
+    reset();
+    
     unsigned char *src_start = src, *src_end = src_start + src_size;
-    auto get_int = [&]() -> unsigned int {
-        unsigned int out = 0, i = 0;
-        while (i < 4) out |= (unsigned int)*(src++) << (8 * i++);
+    auto cmp_str = [&](const char *in1, int length) -> bool {
+        while ((*(src++) == (unsigned char)(*(in1++))) && --length);
+        return !length;
+    }
+    auto get_int = [&]() -> unsigned {
+        unsigned out = 0;
+        for (int i = 0; src < src_end && i < 4; ++i) {
+            out |= (unsigned)*(src++) << (8 * (3 - i));
+        }
         return out;
     };
     
     if (this->isDebug) fprintf(stderr, "    Check LBA for header\n");
-    this->table = {};
-    if (std::string(src, src + 4) != "GIMG") return 0;
+    if (!cmp_str("GIMG", 4)) return 0;
     else src += 4;
+    if ((int)(this->amnt_glba = get_int()) < 0) return 0;
 
-    lbaSpec tmp;
-    src += 4;
-    tmp.glba_amnt = get_int();
-
-    if (this->isDebug) fprintf(stderr, "    Resize LBA table to %d\n", tmp.glba_amnt);
-    tmp.glba_info = new lbaSpec::lbainf[tmp.glba_amnt] {};
+    if (this->isDebug) fprintf(stderr, "    Resize LBA table to %d\n", this->amnt_glba);
+    this->info = new lbainf[this->amnt_glba] {};
 
     if (this->isDebug) fprintf(stderr, "    Update LBA table\n");
-    for (int m = 0; src < src_end && m < tmp.glba_amnt; ++m) {
-        tmp.glba_info[m].file_name = (char*)(src_start + get_int());
-        tmp.glba_info[m].file_rlbn = get_int();
+    for (int m = 0, tmp; src < src_end && m < this->amnt_glba; ++m) {
+        this->info[m].file_name = (char*)(src_start + get_int());
+        this->info[m].file_rlbn = get_int();
         src += 4;
-        tmp.glba_info[m].file_size = get_int();
+        this->info[m].file_size = get_int();
 
         if (this->isDebug) fprintf(stderr, "        Set %-*s with size 0x%08X to entry %d\n",
-                                           27, tmp.glba_info[m].file_name.c_str(),
-                                           tmp.glba_info[m].file_size,
+                                           27, this->info[m].file_name.c_str(),
+                                           this->info[m].file_size,
                                            m);
     }
-    this->table = tmp;
 
     return 1;
 }
@@ -136,13 +141,15 @@ std::string lbat::getTableCSV(unsigned char *src, unsigned src_size) {
 
     std::string gimg = "";
 
-    if (setTableBIN(src, src_size)) {
-        gimg += "GIMG_AMNT: " + std::to_string(table.glba_amnt) + "\n";
+    if (src && src_size) setTableBIN(src, src_size);
+    
+    if (this->info && this->amnt_glba) {
+        gimg += "GIMG_AMNT: " + std::to_string(this->amnt_glba) + "\n";
         gimg += "FILE_NAME, FILE_RLBN, FILE_SIZE\n";
-        for (int g = 0; g < table.glba_amnt; ++g) {
-            gimg += table.glba_info[g].file_name + ", ";
-            gimg += std::to_string(table.glba_info[g].file_rlbn) + ", ";
-            gimg += std::to_string(table.glba_info[g].file_size) + "\n";
+        for (int g = 0; g < this->amnt_glba; ++g) {
+            gimg += this->info[g].file_name + ", ";
+            gimg += std::to_string(this->info[g].file_rlbn) + ", ";
+            gimg += std::to_string(this->info[g].file_size) + "\n";
         }
     }
 
